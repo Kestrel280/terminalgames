440719f920310d7c5196adc4e8a0e79928c917be
Sam Downey <downey.samp@gmail.com>
2025-11-05 10:03:41 -0500

    pong structure

--- pong/pong.c
+++ pong/pong.c
@@ -1,6 +1,94 @@
 #include <stdio.h>
+#include <ncurses.h>
+#include "pong.h"
+
+const int screenW;
+const int screenH;
 
 int main(int argc, char* argv[]) {
-    printf("hello world\n");
+    initscr();
+    initColors();
+    playGame();
+
+    getch();
+    endwin();
+    
     return 0;
+}
+
+/* Setup */
+void initColors() {
+    start_color();
+    if (has_colors() == false) return;
+    init_pair(COLOR_PLAYER_PADDLE, COLOR_PLAYER_PADDLE_FG, COLOR_PLAYER_PADDLE_BG);
+    init_pair(COLOR_OPPONENT_PADDLE, COLOR_OPPONENT_PADDLE_FG, COLOR_OPPONENT_PADDLE_BG);
+}
+
+void initGame(Game* game) {
+    game->state = GAME_STATE_READY;
+    initPlayer(&game->p1, COLOR_PLAYER_PADDLE, 3);
+    initPlayer(&game->p2, COLOR_OPPONENT_PADDLE, screenW - 3);
+    game->finished = false;
+    game->pauseCounter = 100;
+}
+
+void initPlayer(Player* player, int color, int x) {
+    player->score = 0;
+    player->pad.width = 1;
+    player->pad.height = 5;
+    player->pad.y = screenH / 2;
+    player->pad.x = x;
+    player->pad.vx = 0;
+    player->pad.vy = 0;
+    player->pad.colorpair = color;
+}
+
+/* Game logic */
+void playGame() {
+    Game game;
+    initGame(&game);
+    game.state = GAME_STATE_READY;
+
+    while (!game.finished) {
+        switch(game.state) {
+            case GAME_STATE_READY: 
+                if (--game.pauseCounter <= 0) { game.pauseCounter = 100; game.state = GAME_STATE_PLAYING; }
+                break;
+            case GAME_STATE_PLAYING:
+                gameTick(&game);
+                break;
+            case GAME_STATE_SCORED:
+                if (--game.pauseCounter <= 0) { game.pauseCounter = 100; game.state = GAME_STATE_READY; }
+                break;
+        }
+
+        drawGame(game);
+    }
+}
+
+void gameTick(Game* game) {
+    return;
+}
+
+
+/* Drawing */
+void drawPaddle(Paddle pad) {
+    attron(COLOR_PAIR(pad.colorpair));
+    for (int j = 0; j < pad.width; j++) {
+        for (int i = 0; i < pad.height; i++) {
+            mvaddch(i + pad.y, j + pad.x, '#');
+        }
+    }
+    attroff(COLOR_PAIR(pad.colorpair));
+}
+
+void drawBorder() {
+    return;
+}
+
+void drawGame(Game game) {
+    drawBorder();
+    drawPaddle(game.p1.pad);
+    drawPaddle(game.p2.pad);
+    refresh();
 }
---
+++ pong/pong.h
@@ -0,0 +1,71 @@
+#define PADDLE_CH '#'
+#define BALL_CH 'O'
+
+#define COLOR_PLAYER_PADDLE_BG COLOR_WHITE
+#define COLOR_PLAYER_PADDLE_FG COLOR_PLAYER_PADDLE_BG
+#define COLOR_OPPONENT_PADDLE_BG COLOR_RED
+#define COLOR_OPPONENT_PADDLE_FG COLOR_OPPONENT_PADDLE_BG
+
+typedef struct _paddle Paddle;
+typedef struct _ball Ball;
+typedef struct _game Game;
+typedef struct _player Player;
+
+const int screenW = 50;
+const int screenH = 25;
+
+enum {
+    COLOR_UNUSED,
+    COLOR_PLAYER_PADDLE,
+    COLOR_OPPONENT_PADDLE
+};
+
+enum {
+    GAME_STATE_READY,
+    GAME_STATE_PLAYING,
+    GAME_STATE_SCORED
+};
+
+struct _paddle {
+    int width;
+    int height;
+    int y;
+    int x;
+    int vy;
+    int vx;
+    int colorpair;
+};
+
+struct _ball {
+    int y;
+    int x;
+    int vy;
+    int vx;
+};
+
+struct _player {
+    int score;
+    Paddle pad;
+};
+
+struct _game {
+    bool finished;
+    int state;
+    Player p1;
+    Player p2;
+    int pauseCounter;
+};
+
+/* Setup */
+void initColors();
+void initGame(Game* game);
+void initPlayer(Player* player, int color, int x);
+
+/* Game logic */
+void playGame();
+void gameTick(Game* game);
+
+/* Drawing */
+void drawGame(Game game);
+void drawBorder();
+void drawPaddle(Paddle pad);
