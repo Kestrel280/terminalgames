19ad0c65abea56047cd874f8ad86bcf8d85028f4
Sam Downey <downey.samp@gmail.com>
2025-11-06 12:05:50 -0500

    pong everything but ai

--- pong/pong.c
+++ pong/pong.c
@@ -30,20 +30,23 @@ int main(int argc, char* argv[]) {
 void initColors() {
     start_color();
     if (has_colors() == false) return;
+    init_pair(COLOR_BG, COLOR_BACKGROUND_FG, COLOR_BACKGROUND_BG);
     init_pair(COLOR_PLAYER_PADDLE, COLOR_PLAYER_PADDLE_FG, COLOR_PLAYER_PADDLE_BG);
     init_pair(COLOR_OPPONENT_PADDLE, COLOR_OPPONENT_PADDLE_FG, COLOR_OPPONENT_PADDLE_BG);
     init_pair(COLOR_BALL, COLOR_BALL_FG, COLOR_BALL_BG);
     init_pair(COLOR_WALL, COLOR_WALL_FG, COLOR_WALL_BG);
+    init_pair(COLOR_TRIM, COLOR_TRIM_FG, COLOR_TRIM_BG);
 }
 
 void initGame(Game* game) {
     game->state = GAME_STATE_READY;
+    game->lastScore = -1;
     initPlayer(&game->p1, COLOR_PLAYER_PADDLE, 3);
     initPlayer(&game->p2, COLOR_OPPONENT_PADDLE, screenW - 3);
     game->ball.subx = screenW / 2 * BALL_SUBTICKS;
     game->ball.suby = screenH / 2 * BALL_SUBTICKS;
-    //game->ball.x = screenW / 2;
-    //game->ball.y = screenH / 2;
+    game->ball.x = game->ball.subx / BALL_SUBTICKS;
+    game->ball.y = game->ball.suby / BALL_SUBTICKS;
     game->ball.subvx = -2 * BALL_SUBTICKS;
     game->ball.subvy = 1 * BALL_SUBTICKS;
     game->ball.colorpair = COLOR_BALL;
@@ -99,6 +102,35 @@ void playGame() {
     }
 }
 
+void score(Game* game, int player) {
+    if (player == 1) game->p1.score++;
+    else if (player == 2) game->p2.score++;
+    if ((game->p1.score > WIN_SCORE) || (game->p2.score > WIN_SCORE)) winGame(game, player);
+    game->lastScore = player;
+    game->state = GAME_STATE_SCORED;
+    game->pauseCounter = SCORED_TICKS;
+    game->p1.pad.x = 3;
+    game->p2.pad.x = screenW - 3;
+    game->p1.pad.y = screenH / 2;
+    game->p2.pad.y = screenH / 2;
+    game->p1.pad.vy = 0;
+    game->p1.pad.vx = 0;
+    game->p2.pad.vy = 0;
+    game->p2.pad.vx = 0;
+    game->ball.lastCol = -1;
+    game->ball.subx = screenW / 2 * BALL_SUBTICKS;
+    game->ball.suby = screenH / 2 * BALL_SUBTICKS;
+    game->ball.subvx = -2 * BALL_SUBTICKS;
+    game->ball.subvy = 1 * BALL_SUBTICKS;
+    game->ball.x = game->ball.subx / BALL_SUBTICKS;
+    game->ball.y = game->ball.suby / BALL_SUBTICKS;
+    return;
+}
+
+void winGame(Game* game, int player) {
+    game->finished = true;
+}
+
 void gameTick(Game* game) {
     // Update paddle positions, ensuring they stay in bounds
     game->p1.pad.y += game->p1.pad.vy;
@@ -108,7 +140,7 @@ void gameTick(Game* game) {
     game->p1.pad.y = clamp(game->p1.pad.y, BORDER_THICKNESS - 1, screenH - game->p1.pad.height);
     game->p2.pad.y = clamp(game->p2.pad.y, BORDER_THICKNESS - 1, screenH - game->p2.pad.height);
     game->p1.pad.x = clamp(game->p1.pad.x, BORDER_THICKNESS - 1, screenW / 3 - game->p1.pad.width);
-    game->p2.pad.x = clamp(game->p2.pad.x, screenW / 3 + BORDER_THICKNESS - 1, screenW - game->p2.pad.width);
+    game->p2.pad.x = clamp(game->p2.pad.x, 2 * screenW / 3, screenW - game->p2.pad.width);
     game->p1.pad.vy = 0;
     game->p2.pad.vy = 0;
     game->p1.pad.vx = 0;
@@ -146,8 +178,10 @@ void gameTick(Game* game) {
     if (newY <= BORDER_THICKNESS) (game->ball.subvy *= -1);
     else if (newY >= (screenH - BORDER_THICKNESS)) (game->ball.subvy *= -1);
 
-    game->ball.x = newX;
-    game->ball.y = newY;
+    // Check for score
+    if (newX <= 0) score(game, 2);
+    else if (newX >= screenW) score(game, 1);
+    else { game->ball.x = newX; game->ball.y = newY; }
     return;
 }
 
@@ -172,18 +206,30 @@ void gameHandleInput(Game* game) {
 void drawGame(Game game) {
     erase();
     drawBorder();
+    drawScore(game);
+    drawTrim();
     drawPaddle(game.p1.pad, game.frac);
     drawPaddle(game.p2.pad, game.frac);
     drawBall(game.ball, game.frac);
     switch (game.state) {
         case GAME_STATE_READY: drawOverlay("READY"); break;
-        case GAME_STATE_SCORED: drawOverlay("SCORED"); break;
+        case GAME_STATE_SCORED: char buf[20]; sprintf(buf, "P%d SCORED", game.lastScore); buf[19] = '\x00'; drawOverlay(buf); break;
     }
     refresh();
 }
 
 void drawOverlay(const char* msg) {
     mvprintw(screenH / 2, screenW / 2 - strlen(msg) / 2, msg);
+}
+
+void drawTrim() {
+    int x = screenW / 3, y;
+    attron(COLOR_PAIR(COLOR_TRIM));
+    for (y = 1; y < screenH; y++) mvaddch(y, x, '|');
+    x = 2 * screenW / 3;
+    for (y = 1; y < screenH; y++) mvaddch(y, x, '|');
+    attroff(COLOR_PAIR(COLOR_TRIM));
+    return;
 }
 
 void drawBorder() {
@@ -218,5 +264,14 @@ void drawBall(Ball ball, float frac) {
     attron(COLOR_PAIR(ball.colorpair));
     mvaddch(y, x, 'O');
     attroff(COLOR_PAIR(ball.colorpair));
+    return;
+}
+
+void drawScore(Game game) {
+    char buf[20];
+    sprintf(buf, "PLAYER 1: %d", game.p1.score);
+    mvprintw(3, 3, buf);
+    sprintf(buf, "PLAYER 2: %d", game.p2.score);
+    mvprintw(3, screenW - 3 - strlen(buf), buf);
     return;
 }
--- pong/pong.h
+++ pong/pong.h
@@ -1,34 +1,41 @@
 #define PADDLE_CH '#'
 #define BALL_CH 'O'
 
-#define COLOR_BALL_BG COLOR_BLUE
+#define COLOR_BACKGROUND_BG COLOR_CYAN
+#define COLOR_BACKGROUND_FG COLOR_BACKGROUND_BG
+#define COLOR_BALL_BG COLOR_YELLOW
 #define COLOR_BALL_FG COLOR_BALL_BG
-#define COLOR_PLAYER_PADDLE_BG COLOR_WHITE
+#define COLOR_PLAYER_PADDLE_BG COLOR_GREEN
 #define COLOR_PLAYER_PADDLE_FG COLOR_PLAYER_PADDLE_BG
 #define COLOR_OPPONENT_PADDLE_BG COLOR_RED
 #define COLOR_OPPONENT_PADDLE_FG COLOR_OPPONENT_PADDLE_BG
-#define COLOR_WALL_BG COLOR_RED
+#define COLOR_WALL_BG COLOR_CYAN
 #define COLOR_WALL_FG COLOR_WALL_BG
+#define COLOR_TRIM_BG COLOR_WHITE
+#define COLOR_TRIM_FG COLOR_TRIM_BG
 
 typedef struct _paddle Paddle;
 typedef struct _ball Ball;
 typedef struct _game Game;
 typedef struct _player Player;
 
-const int screenW = 80;
+const int screenW = 160;
 const int screenH = 35;
 const uint64_t TICK_TIME_US = 100000;
 const int BORDER_THICKNESS = 2;
 const int READY_TICKS = 10;
-const int SCORED_TICKS = 10;
+const int SCORED_TICKS = 20;
 const int BALL_SUBTICKS = 10;
+const int WIN_SCORE = 5;
 
 enum {
     COLOR_UNUSED,
+    COLOR_BG,
     COLOR_PLAYER_PADDLE,
     COLOR_OPPONENT_PADDLE,
     COLOR_BALL,
-    COLOR_WALL
+    COLOR_WALL,
+    COLOR_TRIM
 };
 
 enum {
@@ -70,6 +77,7 @@ struct _game {
     Player p2;
     Ball ball;
     int pauseCounter;
+    int lastScore;
     float frac; // percentage to next tick, 0-1
 };
 
@@ -80,12 +88,16 @@ void initPlayer(Player* player, int color, int x);
 
 /* Game logic */
 void playGame();
+void score(Game* game, int player);
+void winGame(Game* game, int player);
 void gameTick(Game* game);
 void gameHandleInput(Game* game);
 
 /* Drawing */
 void drawGame(Game game);
 void drawOverlay(const char* msg);
+void drawTrim();
 void drawBorder();
 void drawPaddle(Paddle pad, float frac);
 void drawBall(Ball ball, float frac);
+void drawScore(Game game);
