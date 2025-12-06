8eeb31ffd23c28f425709c6e2483c2d9e60f89ee
Kestrel280 (VPS) <downey.samp@gmail.com>
2025-12-06 03:41:49 +0000

    api remove dummy game

--- api/leaderboard.c
+++ api/leaderboard.c
@@ -9,7 +9,7 @@
 #define EXPAND_AND_LOG_SQL_STATEMENT(stmt) do { char* __expStr__ = sqlite3_expanded_sql(stmt); LOG("\t expanded SQL statement: '%s'\n", __expStr__); sqlite3_free(__expStr__); } while(0)
 
 // games which can be queried
-const char* GAMES[] = { "snake" , "keepaway" , "anotherfakegame" };
+const char* GAMES[] = { "snake" , "keepaway" };
 const char* leaderboardEndpoint = "leaderboards";
 
 static inline bool SAME_STRING(const char* s1, const char* s2) { return (strcmp(s1, s2) == 0); }
